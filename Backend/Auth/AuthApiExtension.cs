b7c21097624937efc0073cd09d0b67e42f872bfe
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-09-29 10:04:59 +0600

    separate endpoints and handlers

--- Backend/Auth/AuthApiExtension.cs
+++ Backend/Auth/AuthApiExtension.cs
@@ -1,66 +1,24 @@
-﻿using Backend.Auth.DTOs;
-using Backend.Auth.Services.Interfaces;
-using Backend.EF.Models;
-
-namespace Backend.Auth
+﻿namespace Backend.Auth
 {
     public static class AuthApiExtension
     {
-        private static void MapLogin(this RouteGroupBuilder builder)
-        {
-            builder.MapPost("/login", (LoginDTO data, IUserService userService, ITokenService tokenService) =>
-            {
-                // находим пользователя
-                User? person = userService.GetPersonWithPassword(data);
-                // если пользователь не найден, отправляем статусный код 401
-                if (person is null) return Results.Unauthorized();
-
-                // создаем токен
-                var token = tokenService.GenerateToken(person.Email, person.Role);
-
-                return Results.Ok(token);
-            })
-                .Produces<string>()
-                .WithName("auth login")
-                .WithDescription("login endpoint");
-        }
-
-        private static void MapRegister(this RouteGroupBuilder builder)
-        {
-            builder.MapPost("/register", (LoginDTO data, IUserService userService) =>
-            {
-                bool created = userService.CreateUser(data);
-
-                if (!created) return Results.Json<string>(statusCode: StatusCodes.Status400BadRequest, data: $"User {data.Email} not created, already exists");
-
-                return Results.Json<string>(statusCode: StatusCodes.Status201Created, data: $"User {data.Email} created!");
-            })
-                .Produces(statusCode: StatusCodes.Status201Created)
-                .WithName("auth register")
-                .WithDescription("register endpoint");
-        }
-
-        private static void MapPasswordChange(this RouteGroupBuilder builder)
-        {
-            builder.MapPost("/changepassword", (PasswordResetDTO data, IUserService userService) =>
-            {
-                bool changed = userService.ChangePassword(data);
-
-                if (!changed) return Results.Json<string>(statusCode: StatusCodes.Status400BadRequest, data: $"Password not changed, bad login data");
-
-                return Results.Ok($"Password changed!");
-            })
-                .WithName("auth change password")
-                .WithDescription("password change endpoint");
-        }
-
         public static RouteGroupBuilder MapAuthEndpoints(this WebApplication app, string authRouteBase = "/api/auth")
         {
             var builder = app.MapGroup(authRouteBase);
 
-            builder.MapLogin();
-            builder.MapRegister();
-            builder.MapPasswordChange();
+            builder.MapPost("/login", AuthEndpointHandlers.Login)
+               .Produces<string>()
+               .WithName("auth login")
+               .WithDescription("login endpoint");
+
+            builder.MapPost("/register", AuthEndpointHandlers.Register)
+                .Produces(statusCode: StatusCodes.Status201Created)
+                .WithName("auth register")
+                .WithDescription("register endpoint");
+
+            builder.MapPost("/changepassword", AuthEndpointHandlers.PasswordChange)
+                .WithName("auth change password")
+                .WithDescription("password change endpoint");
 
             builder
                 .WithTags("auth")
---
+++ Backend/Auth/AuthEndpointHandlers.cs
@@ -0,0 +1,40 @@
+﻿using Backend.Auth.DTOs;
+using Backend.Auth.Services.Interfaces;
+using Backend.EF.Models;
+
+namespace Backend.Auth
+{
+    public static class AuthEndpointHandlers
+    {
+        public static IResult Login(LoginDTO data, IUserService userService, ITokenService tokenService)
+        {
+            // находим пользователя
+            User? person = userService.GetPersonWithPassword(data);
+            // если пользователь не найден, отправляем статусный код 401
+            if (person is null) return Results.Unauthorized();
+
+            // создаем токен
+            var token = tokenService.GenerateToken(person.Email, person.Role);
+
+            return Results.Ok(token);
+        }
+
+        public static IResult Register(LoginDTO data, IUserService userService)
+        {
+            bool created = userService.CreateUser(data);
+
+            if (!created) return Results.Json(statusCode: StatusCodes.Status400BadRequest, data: $"User {data.Email} not created, already exists");
+
+            return Results.Json(statusCode: StatusCodes.Status201Created, data: $"User {data.Email} created!");
+        }
+
+        public static IResult PasswordChange(LoginDTO data, IUserService userService)
+        {
+            bool created = userService.CreateUser(data);
+
+            if (!created) return Results.Json(statusCode: StatusCodes.Status400BadRequest, data: $"User {data.Email} not created, already exists");
+
+            return Results.Json(statusCode: StatusCodes.Status201Created, data: $"User {data.Email} created!");
+        }
+    }
+}
