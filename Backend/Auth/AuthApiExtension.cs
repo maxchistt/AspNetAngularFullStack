9afef510077b34f82d58d522016c7aa7d4bc3fc8
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-10-10 12:18:48 +0600

    api code fix

--- Backend/Auth/AuthApiExtension.cs
+++ Backend/Auth/AuthApiExtension.cs
@@ -1,4 +1,7 @@
-﻿namespace Backend.Auth
+﻿using Backend.Auth.DTOs;
+using Backend.Auth.Services.Interfaces;
+
+namespace Backend.Auth
 {
     public static class AuthApiExtension
     {
@@ -6,17 +9,35 @@
         {
             var builder = app.MapGroup(authRouteBase);
 
-            builder.MapPost("/login", AuthEndpointHandlers.Login)
+            // Login
+            builder.MapPost("/login", (LoginDTO data, IAuthService authService) =>
+            {
+                string? token = authService.Login(data); // пробуем получить токен
+                if (token is null) return Results.Unauthorized(); // если не удалось, отправляем статусный код 401
+                return Results.Ok(token); // возвращаем токен
+            })
                .Produces<string>()
                .WithName("auth login")
                .WithDescription("login endpoint");
 
-            builder.MapPost("/register", AuthEndpointHandlers.Register)
+            // Register
+            builder.MapPost("/register", (LoginDTO data, IAuthService authService) =>
+            {
+                bool created = authService.Register(data);
+                if (!created) return Results.BadRequest($"User {data.Email} not created, already exists");
+                return Results.Json(statusCode: StatusCodes.Status201Created, data: $"User {data.Email} created!");
+            })
                 .Produces(statusCode: StatusCodes.Status201Created)
                 .WithName("auth register")
                 .WithDescription("register endpoint");
 
-            builder.MapPost("/changepassword", AuthEndpointHandlers.PasswordChange)
+            // Change password
+            builder.MapPost("/changepassword", (PasswordResetDTO data, IAuthService authService) =>
+            {
+                bool changed = authService.PasswordChange(data);
+                if (!changed) return Results.BadRequest($"Password not changed, bad login data");
+                return Results.Ok($"Password changed!");
+            })
                 .WithName("auth change password")
                 .WithDescription("password change endpoint");
 
