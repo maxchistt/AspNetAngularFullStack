edfcd53b9bee1becf36c66d7f80791dee33e9764
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-10-13 17:02:19 +0600

    Update InitialData.cs

--- Backend/EF/Initial/InitialData.cs
+++ Backend/EF/Initial/InitialData.cs
@@ -6,8 +6,9 @@ namespace Backend.EF.Initial
     public static class InitialData
     {
         public static readonly User[] Users = {
-            new User() {Id = 1, Email = "tom@gmail.com", Password = "12345", Role = Roles.Admin },
-            new User() {Id = 2, Email = "bob@gmail.com", Password = "55555", Role = Roles.Client },
+            new User() { Id = 1, Email = "Admin", Password = "Admin", Role = Roles.Admin },
+            new User() { Id = 2, Email = "Manager", Password = "Manager", Role = Roles.Worker },
+            new User() { Id = 3, Email = "Client", Password = "Client", Role = Roles.Client },
         };
     }
 }
