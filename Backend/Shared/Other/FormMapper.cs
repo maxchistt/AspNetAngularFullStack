4ff3a34070dff1c7cb1aad6f8cb8b1bdd41d70f1
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-11-02 12:41:18 +0600

    Update FormMapper.cs

--- Backend/Shared/Other/FormMapper.cs
+++ Backend/Shared/Other/FormMapper.cs
@@ -6,77 +6,58 @@ namespace Backend.Shared.Other
     {
         private static readonly Dictionary<Type, List<PropertyInfo>> proplists = new();
 
-        public static T Map<T>(IFormCollection form) where T : new()
+        private static List<PropertyInfo> GetProps<T>() where T : new()
         {
             if (!proplists.ContainsKey(typeof(T)))
                 proplists[typeof(T)] = typeof(T).GetProperties().ToList();
+            return proplists[typeof(T)];
+        }
 
+        private static string? GetStringElement(this IFormCollection form, string name)
+        {
+            if (form.ContainsKey(name))
+                return form[name];
+            if (form.ContainsKey(name.ToLower()))
+                return form[name.ToLower()];
+            return null;
+        }
+
+        private static IFormFile? GetFileElement(this IFormCollection form, string name)
+        {
+            return form.Files.GetFile(name) ?? form.Files.GetFile(name.ToLower());
+        }
+
+        public static T Map<T>(this IFormCollection form) where T : new()
+        {
             T item = new();
 
-            proplists[typeof(T)].ForEach((prop) =>
+            foreach (var prop in GetProps<T>())
             {
                 string name = prop.Name;
                 Type type = prop.PropertyType;
 
-                string? res = null;
-                IFormFile? file = null;
+                string? str = form.GetStringElement(name);
+                IFormFile? file = str is null ? form.GetFileElement(name) : null;
 
-                if (form.ContainsKey(name))
-                {
-                    res = form[name];
-                }
-                else if (form.ContainsKey(name.ToLower()))
-                {
-                    res = form[name.ToLower()];
-                }
-                else
-                {
-                    var f = form.Files.GetFile(name) ?? form.Files.GetFile(name.ToLower());
-                    if (f is not null) file = f;
-                }
+                if (string.IsNullOrEmpty(str) && file is null) break;
 
-                if (string.IsNullOrEmpty(res) && file is null) return;
+                object value = file is null
+                    ? Convert.ChangeType(str, type)!
+                    : file;
 
-                object obj;
-                if (file is null)
-                {
-                    obj = Convert.ChangeType(res, type);
-                }
-                else if (file is IFormFile)
-                {
-                    obj = file;
-                }
-                else
-                {
-                    return;
-                }
-
-                prop.SetValue(item, obj);
-            });
+                prop.SetValue(item, value);
+            };
 
             return item;
         }
 
-        public static bool Validate<T>(IFormCollection form) where T : new()
+        public static bool Validate<T>(this IFormCollection form) where T : new()
         {
-            if (!proplists.ContainsKey(typeof(T)))
-                proplists[typeof(T)] = typeof(T).GetProperties().ToList();
-
-            foreach (var prop in proplists[typeof(T)])
+            foreach (var prop in GetProps<T>())
             {
                 string name = prop.Name;
 
-                bool contains = false;
-
-                if (form.ContainsKey(name) || form.ContainsKey(name.ToLower()))
-                {
-                    contains = true;
-                }
-                else
-                {
-                    var f = form.Files.GetFile(name) ?? form.Files.GetFile(name.ToLower());
-                    if (f is not null) contains = true;
-                }
+                bool contains = form.GetStringElement(name) is not null || form.GetFileElement(name) is not null;
 
                 if (!contains) return false;
             }
