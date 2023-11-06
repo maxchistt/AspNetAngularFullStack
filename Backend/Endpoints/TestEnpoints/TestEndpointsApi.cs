47ea1456e10a6cffd0ee5278518bf727aa75a045
Maxim Chistyakov <maks.novohatsckij@yandex.ru>
2023-11-06 14:04:44 +0600

    test getting username from claims

--- Backend/Endpoints/TestEnpoints/TestEndpointsApi.cs
+++ Backend/Endpoints/TestEnpoints/TestEndpointsApi.cs
@@ -1,7 +1,9 @@
 ﻿using Backend.DTOs.Other;
+using Backend.Services.DAL.Interfaces;
 using Backend.Shared.AuthParams;
 using Backend.Shared.Other;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Backend.Endpoints.TestEnpoints
 {
@@ -10,6 +12,24 @@ namespace Backend.Endpoints.TestEnpoints
         public static RouteGroupBuilder MapTestEndpoints(this WebApplication app, string authRouteBase = "/api")
         {
             var builder = app.MapGroup(authRouteBase);
+
+            builder.MapGet("/GetCurrentUserName", [Authorize] (ClaimsPrincipal claims, IUsersService usersService) =>
+            {
+                if (claims.Identity is null)
+                    return Results.BadRequest("No Identity");
+
+                if (claims.Identity.Name is null)
+                    return Results.BadRequest("No Identity.Name");
+
+                var user = usersService.GetUser(claims.Identity.Name);
+
+                if (user is null)
+                    return Results.BadRequest($"No user found by \'{claims.Identity.Name}\'");
+
+                return Results.Ok(value: $"Claim name: {claims.Identity.Name}, User Id: {user.Id}, User Name: {user.Email}");
+            })
+                .Produces<string>()
+                .WithName("Get current user name");
 
             builder.MapPost("/posttest", [Authorize(Roles = Roles.Combinations.AdminAndWorker)] (TestDataDTO data) =>
             {
